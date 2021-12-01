ba13a5b3a009a20b993a6e3c5fd1b662db07477f
reedas <reedas@users.noreply.github.com>
2021-12-01 00:12:30 +0000

    independent sensor values and mbed os update

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -4,26 +4,18 @@
 
 /* Send Thread */
 
-typedef struct {
-    float    temperature;  /* AD result of measured voltage */
-    float    lightLevel;   /* AD result of measured current */
-    int      cycles;       /* A counter value               */
-} message_t;
-
-static  MemoryPool<message_t, 16> mpool;
-
-
 void sendThread(void)
 {
+    float    temp;  /* AD result of measured voltage */
+    float    lightLev;   /* AD result of measured current */
+    int      cycles;       /* A counter value               */
     uint32_t i = 0;
     while (true) {
         i++; // fake data update
-        message_t *message = mpool.try_alloc();
-        message->temperature = fmod((i * 0.1f) * 33.0f, 40.0f);
-        message->lightLevel = fmod((i * 0.1f) * 11.0f, 100);
-        message->cycles = i;
-        displaySendUpdateSensor(message->temperature, message->lightLevel, message->cycles);
-        mpool.free(message);
+        temp = fmod((i * 0.1f) * 3.3f, 40);
+        lightLev = fmod((i * 0.1f) * 5.5f, 100);
+        cycles = i;
+        displaySendUpdateSensor(temp, lightLev, cycles);
         ThisThread::sleep_for(1s);
     }
 }

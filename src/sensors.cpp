979fba47adadebde4635b39e10a33bc7cf485d6e
nmanley1999 <10742124@cityplym.ac.uk>
2022-02-04 16:35:39 +0000

    update to sensor page to correct errors

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -2,62 +2,77 @@
 #include "display.h"
 #include <cmath>
 
-#define THERM_GND P10_0
-#define THERM_VCC P10_3
+//#define THERM_GND P10_3
+//#define THERM_VCC P10_0
 #define THERM_OUT P10_1
 
 
-/* Reference resistor in series with the thermistor is of 10kohm */
-#define R_REFERENCE                 (float)(10000)
 
-/* A constant of NCP18XH10F03RB thermistor is */
-#define A_COEFF                     (float)(0.000932679f)
-/* A constant of NCP18XH10F03RB thermistor is */
-#define B_COEFF                     (float)(0.000248772f)
-/* A constant of NCP18XH10F03RB thermistor is */
-#define C_COEFF                     (float)(0.0000002041094f)
+/* Reference resistor in series with the thermistor is of 10kohm */
+#define R_REFERENCE                         (float)(10000)
+
+/* A constant of NCP18XH103F03RB thermistor is */
+#define A_COEFF                          (float)(0.0009032679f)
+/* B constant of NCP18XH103F03RB thermistor is */
+#define B_COEFF                          (float)(0.000248772f)
+/* C constant of NCP18XH103F03RB thermistor is */
+#define C_COEFF                          (float)(0.0000002041094f)
 
 /* Zero Kelvin in degree C */
-#define ABSOLUTE_ZERO
+#define ABSOLUTE_ZERO                       (float)(-273.15)
 
+#define LDR_PORT    P10_4
 
 AnalogIn tempVoltage(THERM_OUT);
+AnalogIn lightLevel(LDR_PORT);
+
 /* Send Thread */
-float readTEmp();
+float readTemp();
+float readLight();
 
 void sendThread(void)
 {
     // this is for reading the thermistor
-    DigitalOut ground(THERM_GND);
-    DigitalOut vcc(THERM_VCC);
-    AnalogIn tempVoltage(THERM_OUT);
-    // initialise thermistor power
-    vcc= true;
-    ground = false;
+    // DigitalOut ground(THERM_GND); // Now permanent connection to power
+    // DigitalOut vcc(THERM_VCC);    // so redundant.
+    // initialise the thermistor power
+    // vcc= false;
+    // ground = true;
 
-        float    temp;  // AD result of measured voltage 
+    float    temp;  // AD result of measured voltage 
     float    lightLev;   // AD result of measured current
     int      cycles;       // A counter value               
     uint32_t i = 0;
     while (true) {
-        i++; // fake data update
-        temp = tempVoltage.read() * 3.3f; 
-        lightLev = fmod((i * 0.1f) * 5.5f, 100);
-        cycles = i;
-        displaySendUpdateSensor(temp, lightLev, cycles);
-        ThisThread::sleep_for(1s);
+    //    i++; // fake data update
+        float temperature;
+
+        temperature = readTemp();
+        lightLev = readLight();
+
+    //    cycles = i;
+        displaySendUpdateSensor(TEMP, temperature);
+        displaySendUpdateSensor(LIGHT, lightLev);
+        ThisThread::sleep_for(100ms);
         
     }
 }
 
-float readTemp () {
-    float temperature;
-    float voltRead = tempVoltage.read() * 2.4;
-    float refCurrent = voltage/R_REFERENCE;
-    float resTherm = (3.3f - voltRead) / refCurrent;
-    float lnRt = log(resTherm);
-    float stEqn = A_COEFF + B_COEFF*lnRt + C_COEFF*powe(lnRt, 3);
-    temperature = (1/stEqn) + ABSOLUTE_ZERO +0.05
+float readTemp()
+{
+    float refVoltage = tempVoltage.read() * 2.4; // Range of ADC 0->2*Vref
+    float refCurrent = refVoltage  / R_REFERENCE; // 10k Reference Resistor
+    float thermVoltage = 3.3 - refVoltage;    // Assume supply voltage is 3.3v
+    float thermResistance = thermVoltage / refCurrent; 
+    float logrT = (float32_t)log((float64_t)thermResistance);
+    /* Calculate temperature from the resistance of thermistor using Steinhart-Hart Equation */
+    float stEqn = (float32_t)((A_COEFF) + ((B_COEFF) * logrT) + 
+                             ((C_COEFF) * pow((float64)logrT, (float32)3)));
+    float temperatureC = (float32_t)(((1.0 / stEqn) + ABSOLUTE_ZERO)  + 0.05);
+    return temperatureC;
+}
 
-    return temperature;
+float readLight()
+{
+    return (lightLevel.read())*100.0f;
 }
